46fa30194966aedbd9472c058d8bb63f04fcf705
Documenter.jl <documenter@juliadocs.github.io>
2022-12-11 00:55:30 +0000

    build based on edb6ce3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeneFinder","category":"page"},{"location":"#GeneFinder","page":"Home","title":"GeneFinder","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeneFinder.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeneFinder]","category":"page"}]
+}
