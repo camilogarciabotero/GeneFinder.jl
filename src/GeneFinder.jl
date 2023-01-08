7b33c8f5fcc8dd8156899c26d138c97e0d1c7c6c
Camilo García <ca.garcia2@uniandes.edu.co>
2023-01-08 18:28:39 -0500

    refactor and bug fix

--- src/GeneFinder.jl
+++ src/GeneFinder.jl
@@ -1,18 +1,18 @@
 module GeneFinder
 
-using BioSequences
+using BioSequences  #standard_genetic_code
 using TestItems: @testitem
 
 include("algorithms/simplefinder.jl")
-export simplefinder, simplefinderextended, cdsgenerator, proteingenerator
+export simplefind, simplefind_extended, simplecds_generator, simpleprot_generator
 
 include("types.jl")
-export ORF, Codon, stopcodons
+export ORF, Codon, stopcodons, startcodon, extended_startcodons
 
 include("helpers.jl")
 export eachcodon, hasprematurestop
 
 include("findgenes.jl")
-export locationgenerator, orfgenerator
+export locationgenerator, locationgenerator_extended, orfgenerator, cdsgenerator, proteingenerator
 
 end
--- src/findgenes.jl
+++ src/findgenes.jl
@@ -13,16 +13,23 @@ This function searches the sequence for start codons, and generates ranges of in
 Returns:
     A generator expression that yields ranges of indices corresponding to the locations of ORFs in the `sequence`.
 """
-function locationgenerator(sequence::LongDNA)
+function locationgenerator(sequence::LongDNA; alternative_start::Bool=false)
     seqbound = length(sequence) - 2
-    start_codon_indices = findall(startcodon, sequence)
-    @inbounds begin
-        return(i.start:j+2 for i in start_codon_indices for j in i.start:3:seqbound if sequence[j:j+2] ∈ stopcodons && !hasprematurestop(sequence[i.start:j+2]))
+    if alternative_start == false
+        start_codon_indices = findall(startcodon, sequence)
+        @inbounds begin
+            (i.start:j+2 for i in start_codon_indices for j in i.start:3:seqbound if sequence[j:j+2] ∈ stopcodons && !hasprematurestop(sequence[i.start:j+2]))
+        end
+    else
+        start_codon_indices = findall(extended_startcodons, sequence)
+        @inbounds begin
+            (i:j+2 for i in start_codon_indices for j in i:3:seqbound if sequence[j:j+2] ∈ stopcodons && !hasprematurestop(sequence[i:j+2]))
+        end
     end
 end
 
 """
-    orfgenerator(sequence::LongDNA)
+    orfgenerator(sequence::LongDNA; alternative_start::Bool=false)
 
 Generate ORFs from the given DNA `sequence`.
 
@@ -34,10 +41,10 @@ This function generates ORFs from the forward and reverse complement strands of 
 Returns:
     A generator expression that yields `ORF` objects corresponding to the ORFs in the `sequence`.
 """
-function orfgenerator(sequence::LongDNA)
-    reversedseq = reverse_complement(sequence)
+function orfgenerator(sequence::LongDNA; alternative_start::Bool=false)
+    revseq = reverse_complement(sequence)
     @inbounds begin
-        orfs = (ORF(location, strand) for strand in ['+', '-'] for location in locationgenerator(strand == '+' ? sequence : reversedseq))
+        orfs = (ORF(location, strand) for strand in ['+', '-'] for location in locationgenerator(strand == '+' ? sequence : revseq; alternative_start))
     end
     return orfs
 end
@@ -54,10 +61,26 @@ end
 
     # > 180195.SAMN03785337.LFLS01000089 -> finds only 1 gene in Prodigal (from Pyrodigal tests)
     seq02 = dna"AACCAGGGCAATATCAGTACCGCGGGCAATGCAACCCTGACTGCCGGCGGTAACCTGAACAGCACTGGCAATCTGACTGTGGGCGGTGTTACCAACGGCACTGCTACTACTGGCAACATCGCACTGACCGGTAACAATGCGCTGAGCGGTCCGGTCAATCTGAATGCGTCGAATGGCACGGTGACCTTGAACACGACCGGCAATACCACGCTCGGTAACGTGACGGCACAAGGCAATGTGACGACCAATGTGTCCAACGGCAGTCTGACGGTTACCGGCAATACGACAGGTGCCAACACCAACCTCAGTGCCAGCGGCAACCTGACCGTGGGTAACCAGGGCAATATCAGTACCGCAGGCAATGCAACCCTGACGGCCGGCGACAACCTGACGAGCACTGGCAATCTGACTGTGGGCGGCGTCACCAACGGCACGGCCACCACCGGCAACATCGCGCTGACCGGTAACAATGCACTGGCTGGTCCTGTCAATCTGAACGCGCCGAACGGCACCGTGACCCTGAACACAACCGGCAATACCACGCTGGGTAATGTCACCGCACAAGGCAATGTGACGACTAATGTGTCCAACGGCAGCCTGACAGTCGCTGGCAATACCACAGGTGCCAACACCAACCTGAGTGCCAGCGGCAATCTGACCGTGGGCAACCAGGGCAATATCAGTACCGCGGGCAATGCAACCCTGACTGCCGGCGGTAACCTGAGC"
-    orfs02 = collect(orfgenerator(seq02))
+    orfs02 = collect(orfgenerator(seq02, alternative_start=false))
 
     @test length(orfs02) == 12
     @test collect(orfgenerator(seq02)) == [ORF(29:40, '+'), ORF(137:145, '+'), ORF(164:184, '+'), ORF(173:184, '+'), ORF(236:241, '+'), ORF(248:268, '+'), ORF(362:373, '+'), ORF(470:496, '+'), ORF(551:574, '+'), ORF(569:574, '+'), ORF(581:601, '+'), ORF(695:706, '+')]
+end
+
+function cdsgenerator(sequence::LongDNA; alternative_start::Bool=false)
+    revseq = reverse_complement(sequence)
+    @inbounds begin
+        cds = (i.strand == '+' ? sequence[i.location] : revseq[i.location] for i in orfgenerator(sequence; alternative_start))
+    end
+    return cds
+end
+
+function proteingenerator(sequence::LongDNA; alternative_start::Bool=false, code::GeneticCode = BioSequences.standard_genetic_code)
+    revseq = reverse_complement(sequence)
+    @inbounds begin
+        proteins = (i.strand == '+' ? translate(sequence[i.location]; alternative_start, code) : translate(revseq[i.location]; alternative_start, code) for i in orfgenerator(sequence; alternative_start))
+    end
+    return proteins
 end
 
 # """
@@ -72,5 +95,6 @@ end
 # function findgenes(::SimpleFinder, sequence::LongDNA) # type::GeneticCode
 #     orfs = simplefinder(sequence)
 #     seqs = Vector{CDS}()
-    
+
+
 # end
