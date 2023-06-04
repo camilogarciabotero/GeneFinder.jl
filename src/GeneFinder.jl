70a91e3597f8b74ab27d5e56eaf030cefada1aee
Camilo García <ca.garcia2@uniandes.edu.co>
2023-06-04 18:44:54 -0500

    add precompilation statement

--- src/GeneFinder.jl
+++ src/GeneFinder.jl
@@ -5,6 +5,7 @@ using BioSequences: GeneticCode
 using FASTX: FASTA
 using IterTools
 using TestItems: @testitem
+using PrecompileTools
 
 include("types.jl")
 export ORF, Codon, CDS, Protein
@@ -20,5 +21,18 @@ export write_cds, write_proteins, write_bed, write_gff
 
 include("helpers.jl")
 export fasta_to_dna, sort
+
+
+@setup_workload begin
+    # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
+    # precompile file and potentially make loading faster.
+    using BioSequences
+    seq = randdnaseq(1000)
+    @compile_workload begin
+        # all calls in this block will be precompiled, regardless of whether
+        # they belong to your package or not (on Julia 1.8 and higher)
+        findorfs(seq)
+    end
+end
 
 end
