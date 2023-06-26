6dc622f0b8cbc86c7dbc9c600fdb2182eb52d379
Camilo García <ca.garcia2@uniandes.edu.co>
2023-06-26 18:14:17 -0500

    reformat TPM show

--- src/extended.jl
+++ src/extended.jl
@@ -44,13 +44,13 @@ function Base.show(io::IO, tpm::TPM)
     println(io, "GeneFinder.tpm{$(typeof(tpm.order)), $(typeof(tpm.probabilities)):")
 
     # Print header
-    max_digits = maximum([length(string(maximum(tpm.probabilities[:, i]))) for i in 1:size(tpm.probabilities, 2)])
+    max_digits = maximum([length(string(maximum(round.(tpm.probabilities[:, i], digits = 3)))) for i in 1:size(tpm.probabilities, 2)])
     header_str = "   " * join([rpad(n, max_digits+1) for n in nucleotides], "")
     println(io, header_str)
 
     # Print rows
     for (i, nucleotide1) in enumerate(nucleotides)
-        row = tpm.probabilities[i, :]
+        row = round.(tpm.probabilities[i, :], digits = 3)
         row_str = join([rpad(string(x), max_digits+1) for x in row], "")
         println(io, "$nucleotide1  $row_str")
     end
