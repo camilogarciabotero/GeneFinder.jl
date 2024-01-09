f16b96a5a67cb41e48125627250b966a8f949d72
Camilo García <ca.garcia2@uniandes.edu.co>
2024-01-09 14:17:36 -0500

    Refactor sort and getindex functions in extended.jl

--- src/extended.jl
+++ src/extended.jl
@@ -1,7 +1,8 @@
 # Methods from main packages that expand their fuctions to this package structs
-import Base: show, length, iterate, sort
+import Base: length, iterate, sort, getindex
 
 Base.sort(v::Vector{<:ORF}) = sort(v, by = _orf_sort_key)
+Base.getindex(sequence::NucleicSeqOrView{A}, orf::ORF) where {A} = orf.strand == '+' ? (@view sequence[orf.location]) : reverse_complement(@view sequence[orf.location])
 
 function _orf_sort_key(orf::ORF)
     return (orf.location, orf.strand)
