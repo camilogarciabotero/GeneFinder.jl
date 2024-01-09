b0b3eceab6df4f736fc36649639c688163dd82f2
Camilo García <ca.garcia2@uniandes.edu.co>
2024-01-09 14:30:02 -0500

    Add newline

--- src/extended.jl
+++ src/extended.jl
@@ -3,6 +3,7 @@ import Base: length, iterate, sort, getindex
 
 Base.sort(v::Vector{<:ORF}) = sort(v, by = _orf_sort_key)
 Base.getindex(sequence::NucleicSeqOrView{A}, orf::ORF) where {A} = orf.strand == '+' ? (@view sequence[orf.location]) : reverse_complement(@view sequence[orf.location])
+
 function _orf_sort_key(orf::ORF)
     return (orf.location, orf.strand)
 end
