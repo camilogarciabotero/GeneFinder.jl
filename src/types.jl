655108ff53b3ac894646f5efa2be94a9f5a8f0ea
Camilo García <ca.garcia2@uniandes.edu.co>
2024-01-09 14:17:55 -0500

    Refactor frame type in ORF struct

--- src/types.jl
+++ src/types.jl
@@ -41,5 +41,11 @@ The ORF struct represents an open reading frame in a DNA sequence. It has two fi
 struct ORF <: AbstractGene
     location::UnitRange{Int64}  # Note that it is also called position for gene struct in GenomicAnotations
     strand::Char
-    frame::Int8  # Use Int64 instead of Int
+    frame::Int # Use Int64 instead of Int
+
+    function ORF(location::UnitRange{Int64}, strand::Char, frame::Int)
+        @assert frame in (1, 2, 3) "Invalid frame value. Frame must be 1, 2, or 3."
+        @assert strand in ('+', '-') "Invalid strand value. Strand must be '+' or '-'."
+        new(location, strand, frame)
+    end
 end
