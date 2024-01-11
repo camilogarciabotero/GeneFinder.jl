c4cbcae68fe70b37f8c051e774e629288b657b95
Camilo García <ca.garcia2@uniandes.edu.co>
2024-01-11 11:19:32 -0500

    Remove commented out code in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,13 +11,3 @@ include("iotest.jl")
 include("aquatest.jl")
 
 end
-# @testset "GeneFinder.jl" begin
-#     # Write your tests here.
-# end
-
-# using TestItemRunner: @run_package_tests
-# @run_package_tests
-
-# using Aqua
-# using GeneFinder
-# Aqua.test_all(GeneFinder)
