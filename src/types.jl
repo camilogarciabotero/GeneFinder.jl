838c99c053d9ba18e3712655208251197a65292b
Camilo García <ca.garcia2@uniandes.edu.co>
2024-02-05 22:06:31 -0500

    Update Gene and ORF structs with score field

--- src/types.jl
+++ src/types.jl
@@ -6,10 +6,10 @@ abstract type AbstractGene end
         seqname::String
         start::Int64
         stop::Int64
-        score::Float64
         strand::Char
-        frame::'.' 
-        attribute::
+        frame::Int8
+        score::Union{Float64, Nothing} # Add score field
+        attribute::Dict
     end
 
 This is the main Gene struct, based on the fields that could be found in a GFF3, still needs to be defined correctly,
@@ -20,10 +20,16 @@ This is the main Gene struct, based on the fields that could be found in a GFF3,
 #     seqname::String
 #     start::Int64
 #     stop::Int64
-#     score::Float64
 #     strand::Char
 #     frame::Int8 # But maybe a Union to allow empty when reading a GFF? 
-#     attribute::Dict # Should be a Dict perhaps
+#     score::Union{Float64, Nothing} # Add score field
+#     attribute::Dict # Should be a Dict perhaps or a NamedTuple
+# end
+
+# struct GeneFeatures <: AbstractGene
+#     seqname::String could be a Union{String, Nothing} and named id.
+#     orf::ORF
+#     attribute::Dict
 # end
 
 """
@@ -31,21 +37,29 @@ This is the main Gene struct, based on the fields that could be found in a GFF3,
         location::UnitRange{Int64}
         strand::Char
         frame::Int
+        score::Union{Float64, Nothing} # Add score field
     end
 
 The ORF struct represents an open reading frame in a DNA sequence. It has two fields: 
 
 - `location`: which is a UnitRange{Int64} indicating the start and end locations of the ORF in the sequence
 - `strand`:  is a Char type indicating whether the ORF is on the forward ('+') or reverse ('-') strand of the sequence.
+- `frame`: is an Int type indicating the reading frame of the ORF. The frame is the position of the first nucleotide of the codon that starts the ORF, relative to the start of the sequence. It can be 1, 2, or 3.
+- `score`: is a Union{Float64, Nothing} type indicating the score of the ORF. It can be a Float64 or nothing.
 """
 struct ORF <: AbstractGene
     location::UnitRange{Int64}  # Note that it is also called position for gene struct in GenomicAnotations
     strand::Char
     frame::Int # Use Int64 instead of Int
+    score::Union{Float64, Nothing} # Add score field
 
-    function ORF(location::UnitRange{Int64}, strand::Char, frame::Int)
+    function ORF(location::UnitRange{Int64}, strand::Char, frame::Int, score::Union{Float64, Nothing} = nothing)
         @assert frame in (1, 2, 3) "Invalid frame value. Frame must be 1, 2, or 3."
         @assert strand in ('+', '-') "Invalid strand value. Strand must be '+' or '-'."
-        new(location, strand, frame)
+        @assert location[1] < location[end] "Invalid location. Start must be less than stop."
+        @assert score isa Union{Float64, Nothing} "Invalid score value. Score must be a Float64 or nothing."
+    
+        return new(location, strand, frame, score)
     end
 end
+
