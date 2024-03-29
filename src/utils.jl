2fdae66c47d1a9ef9457981bdf80ed2e02fde5e5
Camilo García <ca.garcia2@uniandes.edu.co>
2024-03-29 18:07:08 -0500

    Remove commented out code and unused function

--- src/utils.jl
+++ src/utils.jl
@@ -1,21 +1,5 @@
 # export fasta_to_dna, hasprematurestop
 # General purposes methods supporting main functions
-# """
-#     fasta_to_dna(input::String)
-
-# Converts a FASTA formatted file (even if it is a multi-fasta) to an array of `LongSequence{DNAAlphabet{4}}` objects.
-# """
-# function fasta_to_dna(input::AbstractString)::Vector{LongSequence{DNAAlphabet{4}}}
-#     FASTAReader(open(input)) do reader
-#         return [LongSequence{DNAAlphabet{4}}(sequence(record)) for record in reader]
-#     end
-# end
-
-# function gff_to_dna(input::AbstractString)
-#     GFF3.Reader(open(input)) do reader
-#         return [record for record in reader]
-#     end
-# end
 
 """
     hasprematurestop(sequence::LongNucOrView{4})::Bool
