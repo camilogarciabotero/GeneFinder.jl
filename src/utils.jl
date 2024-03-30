6cbfdeb66a7ff766cf163694cda06998d7f05336
Camilo García <ca.garcia2@uniandes.edu.co>
2024-03-29 20:01:50 -0500

    Comment again another method

--- src/utils.jl
+++ src/utils.jl
@@ -8,23 +8,23 @@ Determine whether the `sequence` of type `LongSequence{DNAAlphabet{4}}` contains
 
 Returns a boolean indicating whether the `sequence` has more than one stop codon.
 """
-function hasprematurestop(sequence::NucleicSeqOrView{DNAAlphabet{N}})::Bool where {N}
+# function hasprematurestop(sequence::NucleicSeqOrView{DNAAlphabet{N}})::Bool where {N}
     
-    stopcodons = [LongDNA{4}("TAA"), LongDNA{4}("TAG"), LongDNA{4}("TGA")]  # Create a set of stop codons
+#     stopcodons = [LongDNA{4}("TAA"), LongDNA{4}("TAG"), LongDNA{4}("TGA")]  # Create a set of stop codons
     
-    length(sequence) % 3 == 0 || error("The sequence is not divisible by 3")
+#     length(sequence) % 3 == 0 || error("The sequence is not divisible by 3")
     
-    occursin(biore"T(AG|AA|GA)"dna, sequence[end-2:end]) || error("There is no stop codon at the end of the sequence")
+#     occursin(biore"T(AG|AA|GA)"dna, sequence[end-2:end]) || error("There is no stop codon at the end of the sequence")
 
-    @inbounds for i in 1:3:length(sequence) - 4
-        codon = sequence[i:i+2]
-        if codon in stopcodons
-            return true
-        end
-    end
+#     @inbounds for i in 1:3:length(sequence) - 4
+#         codon = sequence[i:i+2]
+#         if codon in stopcodons
+#             return true
+#         end
+#     end
 
-    return false
-end
+#     return false
+# end
 
 @doc raw"""
     iscoding(
