9132e110496e0658fef945a8712d53bea2cdd15a
Camilo García <ca.garcia2@uniandes.edu.co>
2024-05-23 17:43:52 -0500

    refactor: Remove unused code in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,20 +15,4 @@ include("findorfstest.jl")
 include("getindextest.jl")
 # include("aquatest.jl")
 
-
-
-@testitem "getorfs dna" default_imports=false begin
-
-    using BioSequences: @dna_str, DNAAlphabet
-    using GeneFinder: NaiveFinder, getorfs, findorfs, ORF
-    using Test: @test
-
-    seq01 = dna"ATGATGCATGCATGCATGCTAGTAACTAGCTAGCTAGCTAGTAA"
-    orfseqs = getorfs(seq01, DNAAlphabet{4}(), NaiveFinder())
-
-    # @test length(orfseqs) == 5
-    # @test orfseqs[1] == dna"ATGATGCATGCATGCATGCTAGTAACTAGCTAG"
-end
-
-
 end
