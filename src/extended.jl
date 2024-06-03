61ac5c79e396f8d5ea4483c8a53d850c18d794ad
Camilo García <ca.garcia2@uniandes.edu.co>
2024-06-03 00:42:28 -0500

    Up translate and other utils are now more stable

--- src/extended.jl
+++ src/extended.jl
@@ -1,28 +1,10 @@
 # Methods from main packages that expand their fuctions to this package structs
-import Base: isless, iterate, sort, getindex
+import Base: isless, iterate, sort, getindex, length
+import BioSequences: translate
 
+Base.isless(a::ORF{N,F}, b::ORF{N,F}) where {N,F} = isless(a.first:a.last, b.first:b.last)
 
-# Base.isless(a::ORF, b::ORF) = isless(a.location, b.location)
-Base.isless(a::ORF, b::ORF) = isless(a.first:a.last, b.first:b.last)
-# Base.isless(a::ORF, b::ORF) = isless(a.location, b.location)
-# Base.isless(a::ORF, b::ORF) = isless(a.score, b.score)
-
-# Base.sort(v::Vector{<:ORF}; kwargs...) = sort(v, by = _orf_sort_key)
-# Base.sort!(v::Vector{<:ORF}; kwargs...) = sort!(v, by = _orf_sort_key)
-
-
-#TODOs: how to make more robust the getindex method? confroning the frames?
-# Base.getindex(sequence::NucleicSeqOrView{A}, orf::ORF) where {A} = orf.strand == '+' ? (@view sequence[orf.location]) : reverse_complement(@view sequence[orf.location])
-
-# function getindex(sequence::NucleicSeqOrView{A}, orf::ORF) where {A}
-#     if orf.strand == '+'
-#         return @view sequence[orf.location]
-#     else
-#         return reverse_complement(@view sequence[orf.location])
-#     end
-# end
-
-function getindex(sequence::NucleicSeqOrView{A}, orf::ORF) where {A}
+function getindex(sequence::NucleicSeqOrView{A}, orf::ORF{N,F}) where {A,N,F}
     if orf.strand == '+' || orf.strand == STRAND_POS
         return @view sequence[orf.first:orf.last]
     else
@@ -30,6 +12,6 @@ function getindex(sequence::NucleicSeqOrView{A}, orf::ORF) where {A}
     end
 end
 
-# function _orf_sort_key(orf::ORF)
-#     return (orf.location, orf.strand, orf.score)
-# end
+function translate(orf::ORF{N,F}; kwargs...) where {N,F}
+    return translate(sequence(orf); kwargs...)
+end
