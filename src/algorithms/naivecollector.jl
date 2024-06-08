44c791cae43e03393a1dff35a348b3d850fff477
Camilo García <ca.garcia2@uniandes.edu.co>
2024-06-08 17:44:58 -0500

    Update naivecolletor.jl to new BMC losr

--- src/algorithms/naivecollector.jl
+++ src/algorithms/naivecollector.jl
@@ -32,7 +32,7 @@ function NaiveCollector(
             frame = framedict[(seqlen - x.captured[3]) % 3]
         end
         seq = strand == '+' ? @view(sequence[start:stop]) : reverse_complement(@view(sequence[start:stop])) #@view(sequence[start:stop])
-        scr = scheme === nothing ? 0.0 : scheme(seq, ECOLICDS)
+        scr = scheme === nothing ? 0.0 : scheme(seq; kwargs...)
         # fts = Dict(:score => score)
         fts = Features((score = scr,)) # rbs = RBS(biore"RRR"dna, 3:4, 1.0)
         return ORF{N,NaiveCollector}(seqname, start, stop, strand, frame, seq, fts, scheme)
