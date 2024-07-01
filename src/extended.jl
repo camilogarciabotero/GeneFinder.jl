91cb0bb9ad3a426db712f86262815c0abff1d334
Camilo García <ca.garcia2@uniandes.edu.co>
2024-07-01 14:13:05 -0500

    Update getindex of ORFs

--- src/extended.jl
+++ src/extended.jl
@@ -4,12 +4,20 @@ import BioSequences: translate
 
 Base.isless(a::ORF{N,F}, b::ORF{N,F}) where {N,F} = isless(a.first:a.last, b.first:b.last)
 
-function getindex(sequence::NucleicSeqOrView{A}, orf::ORF{N,F}) where {A,N,F}
-    if orf.strand == '+' || orf.strand == STRAND_POS
-        return @view sequence[orf.first:orf.last]
+function getindex(seq::NucleicSeqOrView{A}, orf::ORF{N,F}) where {A,N,F}
+    @assert @view(seq[begin:end]) == source(orf) "The source sequence of the ORF and the given sequence are different"
+
+    if orf.strand == STRAND_POS
+        s = @view seq[orf.first:orf.last]
     else
-        return reverse_complement(@view sequence[orf.first:orf.last])
+        s = reverse_complement(@view seq[orf.first:orf.last])
     end
+
+    if !occursin(biore"T(AG|AA|GA)"dna, s)
+        error("There is no stop codon at the end of the sequence/orf")
+    end
+
+    return s
 end
 
 function translate(orf::ORF{N,F}; kwargs...) where {N,F}
