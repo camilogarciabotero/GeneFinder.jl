6b04275d2e029eb9edbf1a0edc680151dff89023
Camilo García <ca.garcia2@uniandes.edu.co>
2024-07-02 10:44:39 -0500

    Add more precompilation

--- src/workload.jl
+++ src/workload.jl
@@ -1,13 +1,13 @@
 using PrecompileTools: @setup_workload, @compile_workload
 
-@setup_workload begin
+@setup_workload let
     # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
     # precompile file and potentially make loading faster.
     # using BioSequences, GeneFinder
     seq = randdnaseq(99)
-    @compile_workload begin
+    @compile_workload let
         _locationiterator(seq; alternative_start=false)
-        # findorfs(seq)
+        findorfs(seq)
         # all calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
         # findorfs(seq)
