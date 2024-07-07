47ed7c8f094674cb06a82cfff06e11c07f3a346c
Camilo García <ca.garcia2@uniandes.edu.co>
2024-07-07 11:29:28 -0500

    feat: Refactor iotest.jl and add new tests for writting methods

--- test/iotest.jl
+++ test/iotest.jl
@@ -1,48 +1,48 @@
-# Test the write_orfs_* methods
-# @testset "write_orfs_f* " begin
-    
-#     # seq01 = dna"ATGATGCATGCATGCATGCTAGTAACTAGCTAGCTAGCTAGTAA"
-#     # seq02 = dna"AACCAGGGCAATATCAGTACCGCGGGCAATGCAACCCTGACTGCCGGCGGTAACCTGAACAGCACTGGCAATCTGACTGTGGGCGGTGTTACCAACGGCACTGCTACTACTGGCAACATCGCACTGACCGGTAACAATGCGCTGAGCGGTCCGGTCAATCTGAATGCGTCGAATGGCACGGTGACCTTGAACACGACCGGCAATACCACGCTCGGTAACGTGACGGCACAAGGCAATGTGACGACCAATGTGTCCAACGGCAGTCTGACGGTTACCGGCAATACGACAGGTGCCAACACCAACCTCAGTGCCAGCGGCAACCTGACCGTGGGTAACCAGGGCAATATCAGTACCGCAGGCAATGCAACCCTGACGGCCGGCGACAACCTGACGAGCACTGGCAATCTGACTGTGGGCGGCGTCACCAACGGCACGGCCACCACCGGCAACATCGCGCTGACCGGTAACAATGCACTGGCTGGTCCTGTCAATCTGAACGCGCCGAACGGCACCGTGACCCTGAACACAACCGGCAATACCACGCTGGGTAATGTCACCGCACAAGGCAATGTGACGACTAATGTGTCCAACGGCAGCCTGACAGTCGCTGGCAATACCACAGGTGCCAACACCAACCTGAGTGCCAGCGGCAATCTGACCGTGGGCAACCAGGGCAATATCAGTACCGCGGGCAATGCAACCCTGACTGCCGGCGGTAACCTGAGC"
+@testitem "writting methods" begin
+    cd(@__DIR__) # Required to find the fasta file    
+    using BioSequences, GeneFinder, FASTX
 
-#     # Test case 1
+    function fasta2bioseq(input::AbstractString)::Vector{LongSequence{DNAAlphabet{4}}}
+        FASTAReader(open(input)) do reader
+            return [LongSequence{DNAAlphabet{4}}(FASTX.sequence(record)) for record in reader]
+        end
+    end
+# 
+    # Test case 1 fna
+# 
+    # From pyrodigal issue #13 link: https://github.com/althonos/pyrodigal/blob/1f939b0913b48dbaa55d574b20e124f1b8323825/pyrodigal/tests/test_orf_finder.py#L271
+    # Pyrodigal predicts 2 genes from this sequence:
+    # 1) An alternative start codon (GTG) sequence at 48:347
+    # 2) A common start codon sequence at 426:590
+    # On the other hand, the NCBI ORFfinder program predicts 9 ORFs whose length is greater than 75 nt, from which one has an "outbound" stop
+    seq = dna"TTCGTCAGTCGTTCTGTTTCATTCAATACGATAGTAATGTATTTTTCGTGCATTTCCGGTGGAATCGTGCCGTCCAGCATAGCCTCCAGATATCCCCTTATAGAGGTCAGAGGGGAACGGAAATCGTGGGATACATTGGCTACAAACTTTTTCTGATCATCCTCGGAACGGGCAATTTCGCTTGCCATATAATTCAGACAGGAAGCCAGATAACCGATTTCATCCTCACTATCGACCTGAAATTCATAATGCATATTACCGGCAGCATACTGCTCTGTGGCATGAGTGATCTTCCTCAGAGGAATATATACGATCTCAGTGAAAAAGATCAGAATGATCAGGGATAGCAGGAACAGGATTGCCAGGGTGATATAGGAAATATTCAGCAGGTTGTTACAGGATTTCTGAATATCATTCATATCAGTATGGATGACTACATAGCCTTTTACCTTGTAGTTGGAGGTAATGGGAGCAAATACAGTAAGTACATCCGAATCAAAATTACCGAAGAAATCACCAACAATGTAATAGGAGCCGCTGGTTACGGTCGAATCAAAATTCTCAATGACAACCACATTCTCCACATCTAAGGGACTATTGGTATCCAGTACCAGTCGTCCGGAGGGATTGATGATGCGAATCTCGGAATTCAGGTAGACCGCCAGGGAGTCCAGCTGCATTTTAACGGTCTCCAAAGTTGTTTCACTGGTGTACAATCCGCCGGCATAGGTTCCGGCGATCAGGGTTGCTTCGGAATAGAGACTTTCTGCCTTTTCCCGGATCAGATGTTCTTTGGTCATATTGGGAACAAAAGTTGTAACAATGATGAAACCAAATACACCAAAAATAAAATATGCGAGTATAAATTTTAGATAAAGTGTTTTTTTCATAACAAATCCTGCTTTTGGTATGACTTAATTACGTACTTCGAATTTATAGCCGATGCCCCAGATGGTGCTGATCTTCCAGTTGGCATGATCCTTGATCTTCTC"
+    # 
+    seqfna = "data/out-seq.fna"
+    open(seqfna, "w") do io
+        write_orfs_fna(seq, io, finder=NaiveFinder)
+    end
+# 
+    seqfnarecords = open(collect, FASTAReader, "data/out-seq.fna")
+# 
+    @test seqfnarecords[1] == FASTX.FASTA.Record("unnamedseq id=01 start=5 stop=22 strand=- frame=2 score=0.0", "ATGAAACAGAACGACTGA")
+    @test length(seqfnarecords) == 32
+    @test identifier(seqfnarecords[1]) == "unnamedseq"
+    @test description(seqfnarecords[1]) == "unnamedseq id=01 start=5 stop=22 strand=- frame=2 score=0.0"
+    @test FASTX.sequence(seqfnarecords[1]) == "ATGAAACAGAACGACTGA"
+# 
+    # Test case 2 faa
 
-#     # From pyrodigal issue #13 link: https://github.com/althonos/pyrodigal/blob/1f939b0913b48dbaa55d574b20e124f1b8323825/pyrodigal/tests/test_orf_finder.py#L271
-#     # Pyrodigal predicts 2 genes from this sequence:
-#     # 1) An alternative start codon (GTG) sequence at 48:347
-#     # 2) A common start codon sequence at 426:590
-#     # On the other hand, the NCBI ORFfinder program predicts 9 ORFs whose length is greater than 75 nt, from which one has an "outbound" stop
-#     seq03 = dna"TTCGTCAGTCGTTCTGTTTCATTCAATACGATAGTAATGTATTTTTCGTGCATTTCCGGTGGAATCGTGCCGTCCAGCATAGCCTCCAGATATCCCCTTATAGAGGTCAGAGGGGAACGGAAATCGTGGGATACATTGGCTACAAACTTTTTCTGATCATCCTCGGAACGGGCAATTTCGCTTGCCATATAATTCAGACAGGAAGCCAGATAACCGATTTCATCCTCACTATCGACCTGAAATTCATAATGCATATTACCGGCAGCATACTGCTCTGTGGCATGAGTGATCTTCCTCAGAGGAATATATACGATCTCAGTGAAAAAGATCAGAATGATCAGGGATAGCAGGAACAGGATTGCCAGGGTGATATAGGAAATATTCAGCAGGTTGTTACAGGATTTCTGAATATCATTCATATCAGTATGGATGACTACATAGCCTTTTACCTTGTAGTTGGAGGTAATGGGAGCAAATACAGTAAGTACATCCGAATCAAAATTACCGAAGAAATCACCAACAATGTAATAGGAGCCGCTGGTTACGGTCGAATCAAAATTCTCAATGACAACCACATTCTCCACATCTAAGGGACTATTGGTATCCAGTACCAGTCGTCCGGAGGGATTGATGATGCGAATCTCGGAATTCAGGTAGACCGCCAGGGAGTCCAGCTGCATTTTAACGGTCTCCAAAGTTGTTTCACTGGTGTACAATCCGCCGGCATAGGTTCCGGCGATCAGGGTTGCTTCGGAATAGAGACTTTCTGCCTTTTCCCGGATCAGATGTTCTTTGGTCATATTGGGAACAAAAGTTGTAACAATGATGAAACCAAATACACCAAAAATAAAATATGCGAGTATAAATTTTAGATAAAGTGTTTTTTTCATAACAAATCCTGCTTTTGGTATGACTTAATTACGTACTTCGAATTTATAGCCGATGCCCCAGATGGTGCTGATCTTCCAGTTGGCATGATCCTTGATCTTCTC"
-    
-#     seq03fna = "data/out-seq03.fna"
-#     open(seq03fna, "w") do io
-#         write_orfs_fna(seq03, io, NaiveFinder())
-#     end
+    seqfaa = "data/out-seq.faa"
+    open(seqfaa, "w") do io
+        write_orfs_faa(seq, io, finder=NaiveFinder)
+    end
+# 
+    seqfaarecords = open(collect, FASTAReader, "data/out-seq.faa")
+# 
+    @test seqfaarecords[2] == FASTX.FASTA.Record("unnamedseq id=02 start=37 stop=156 strand=+ frame=1 score=0.0", "MYFSCISGGIVPSSIASRYPLIEVRGERKSWDTLATNFF*")
+    @test length(seqfaarecords) == 32
+    @test identifier(seqfaarecords[2]) == "unnamedseq"
+    @test description(seqfaarecords[2]) == "unnamedseq id=02 start=37 stop=156 strand=+ frame=1 score=0.0"
+    @test FASTX.sequence(seqfaarecords[2]) == "MYFSCISGGIVPSSIASRYPLIEVRGERKSWDTLATNFF*"
 
-#     seq03fnarecords = open(collect, FASTAReader, "data/out-seq03.fna")
-
-#     @test seq03fnarecords[1] == FASTX.FASTA.Record("ORF01 id=01 start=5 stop=22 strand=- frame=2", "ATGAAACAGAACGACTGA")
-#     @test length(seq03fnarecords) == 32
-#     @test identifier(seq03fnarecords[1]) == "ORF01"
-#     @test description(seq03fnarecords[1]) == "ORF01 id=01 start=5 stop=22 strand=- frame=2"
-#     @test sequence(seq03fnarecords[1]) == "ATGAAACAGAACGACTGA"
-
-#     # Test case 2
-
-#     seq03faa = "data/out-seq03.faa"
-#     open(seq03faa, "w") do io
-#         write_orfs_faa(seq03, io, NaiveFinder())
-#     end
-
-#     seq03faarecords = open(collect, FASTAReader, "data/out-seq03.faa")
-
-#     @test seq03faarecords[2] == FASTX.FASTA.Record("ORF02 id=02 start=37 stop=156 strand=+ frame=1", "MYFSCISGGIVPSSIASRYPLIEVRGERKSWDTLATNFF*")
-#     @test length(seq03faarecords) == 32
-#     @test identifier(seq03faarecords[2]) == "ORF02"
-#     @test description(seq03faarecords[2]) == "ORF02 id=02 start=37 stop=156 strand=+ frame=1"
-#     @test sequence(seq03faarecords[2]) == "MYFSCISGGIVPSSIASRYPLIEVRGERKSWDTLATNFF*"
-
-#     # Test case 3 
-
-#     @test seq03faarecords[3] == FASTX.FASTA.Record("ORF03 id=03 start=107 stop=136 strand=- frame=2", "MYPTISVPL*")
-
-# end
+end
