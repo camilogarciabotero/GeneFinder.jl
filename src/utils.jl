de9ac528a2000025248d05fd3df9d2023b4d4394
Camilo García <ca.garcia2@uniandes.edu.co>
2024-07-12 23:48:28 -0500

    Refactor utils.jl for improved readability and variable naming

--- src/utils.jl
+++ src/utils.jl
@@ -1,26 +1,24 @@
-export hasprematurestop, _varname, _varsymbol #fasta2bioseq
+export hasprematurestop, _varname, _varsymbol, _orfseq #fasta2bioseq
 
 # General purposes methods supporting main functions
 
 """
-    hasprematurestop(sequence::LongNucOrView{4})::Bool
+    hasprematurestop(seq::LongNucOrView{4})::Bool
 
 Determine whether the `sequence` of type `LongSequence{DNAAlphabet{4}}` contains a premature stop codon.
 
 Returns a boolean indicating whether the `sequence` has more than one stop codon.
 """
-function hasprematurestop(sequence::NucleicSeqOrView{DNAAlphabet{N}})::Bool where {N}
+function hasprematurestop(seq::NucleicSeqOrView{DNAAlphabet{N}})::Bool where {N}
     
     stopcodons = [LongDNA{4}("TAA"), LongDNA{4}("TAG"), LongDNA{4}("TGA")]  # Create a set of stop codons
-    
-    length(sequence) % 3 == 0 || error("The sequence is not divisible by 3")
+    length(seq) % 3 == 0 || error("The sequence is not divisible by 3")
     
     #TODO: this way of checking the stop codon at the end is idiosyncratic and should be improved since other stop codons are not considered
-    #TODO: This is currently used by the `getindex` method in `extended.jl`
-    occursin(biore"T(AG|AA|GA)"dna, sequence[end-2:end]) || error("There is no stop codon at the end of the sequence/orf")
+    occursin(biore"T(AG|AA|GA)"dna, @view(seq[end-2:end])) || error("There is no stop codon at the end of the sequence/orf")
 
-    @inbounds for i in 1:3:length(sequence) - 4
-        codon = sequence[i:i+2]
+    @inbounds for i in 1:3:length(seq) - 4
+        codon = seq[i:i+2]
         if codon in stopcodons
             return true
         end
@@ -30,16 +28,23 @@ function hasprematurestop(sequence::NucleicSeqOrView{DNAAlphabet{N}})::Bool wher
 end
 
 """
-    fasta2bioseq(input::String)
+    _orfseq(seq::LongSequence{DNAAlphabet{N}}, start::Int64, stop::Int64, strand::Strand) where {N}
 
-Converts a FASTA formatted file (even if it is a multi-fasta) to an array of `LongSequence{DNAAlphabet{4}}` objects.
+Extract the ORF sequence from the `seq` of type `LongSequence{DNAAlphabet{N}}` given the `start` and `stop` positions and the `strand`.
 """
-# function fasta2bioseq(input::AbstractString)::Vector{LongSequence{DNAAlphabet{4}}}
-#     FASTAReader(open(input)) do reader
-#         return [LongSequence{DNAAlphabet{4}}(sequence(record)) for record in reader]
-#     end
-# end
+function _orfseq(seq::LongSequence{DNAAlphabet{N}}, start::Int64, stop::Int64, strand::Strand) where {N}
+    if strand == STRAND_POS
+        return convert(LongSubSeq, seq[start:stop])
+    else
+        return convert(LongSubSeq, reverse_complement(seq[start:stop]))
+    end
+end
 
+"""
+    _varname(var::Any)
+
+Get the name of the variable `var` in the current scope.
+"""
 function _varname(var::Any)
     for name in names(Main)
         try
@@ -54,6 +59,11 @@ function _varname(var::Any)
     return nothing
 end
 
+"""
+    _varsymbol(var::Any)
+
+Get the symbol of the variable `var` in the current scope.
+"""
 function _varsymbol(var::Any)
     for name in names(Main)
         try
