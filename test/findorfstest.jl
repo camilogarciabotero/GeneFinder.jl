5e148eac61ad413aeafbe2de1d6033d8c0cb2d88
Camilo García <ca.garcia2@uniandes.edu.co>
2024-07-15 11:15:29 -0500

    Refactor code for improved readability and variable naming

--- test/findorfstest.jl
+++ test/findorfstest.jl
@@ -3,17 +3,26 @@
     using BioSequences, GeneFinder
     # A random seq to start
 
-    orflarge = ORF{4,NaiveFinder}("seq01", 1, 33, STRAND_POS, 1, Features((score = 0.0,)), nothing)
-    orfshtort = ORF{NaiveFinder}(1:33, '+', 1)
+    seq = dna"ATGATGCATGCATGCATGCTAGTAACTAGCTAGCTAGCTAGTAA"
 
-    # Missing tests:
-    # getindex(seq, orfs.first:orfs.last)
-    # source(orf01)
-    # seq01[orf01]
-    # sequence(orf01) == dna"ATGATGCATGCATGCATGCTAGTAACTAGCTAG"
-    # groupname(orf01) == "seq01"
-    # @view(seq01[begin:end])
-    # GeneFinder.source(orf01) 
+    orflarge = ORF{4,NaiveFinder}("seq", 1, 33, STRAND_POS, 1, convert(LongSubSeq, dna"ATGATGCATGCATGCATGCTAGTAACTAGCTAG"), NamedTuple())
+    orfshtort = ORF{4,NaiveFinder}("seq", 16,33, STRAND_POS, 1, _orfseq(seq, 16, 33, STRAND_POS), NamedTuple()) # requires _orient function and source sequence
+
+    @test orflarge.seq == dna"ATGATGCATGCATGCATGCTAGTAACTAGCTAG"
+    @test orfshtort.seq == dna"ATGCTAGTAACTAGCTAG"
+    
+    @test sequence(orflarge) == dna"ATGATGCATGCATGCATGCTAGTAACTAGCTAG"
+    @test sequence(orfshtort) == dna"ATGCTAGTAACTAGCTAG"
+
+    @test getindex(seq, orflarge.first:orflarge.last) == orflarge.seq
+    @test getindex(seq, orfshtort.first:orfshtort.last) == orfshtort.seq
+
+    @test translate(orflarge) == aa"MMHACMLVTS*"
+    @test translate(orfshtort) == aa"MLVTS*"
+
+    @test length(orflarge) == 33
+    @test length(orfshtort) == 18
+    
 end
 
 @testitem "NaiveFinder overlaps" begin
@@ -22,16 +31,15 @@ end
     seq = dna"ATGATGCATGCATGCATGCTAGTAACTAGCTAGCTAGCTAGTAA"
     orfs = findorfs(seq)
 
-
     orfstest = [
-        ORF{4,NaiveFinder}("seq01", 1, 33, STRAND_POS, 1, Features((score = 0.0,)), nothing),
-        ORF{4,NaiveFinder}("seq01", 4, 33, STRAND_POS, 1, Features((score = 0.0,)), nothing),
-        ORF{4,NaiveFinder}("seq01", 8, 22, STRAND_POS, 2, Features((score = 0.0,)), nothing),
-        ORF{4,NaiveFinder}("seq01", 12,29, STRAND_POS, 3, Features((score = 0.0,)), nothing),
-        ORF{4,NaiveFinder}("seq01", 16,33, STRAND_POS, 1, Features((score = 0.0,)), nothing)
+        ORF{4,NaiveFinder}("seq", 1, 33, STRAND_POS, 1, convert(LongSubSeq, dna"ATGATGCATGCATGCATGCTAGTAACTAGCTAG"), NamedTuple()),
+        ORF{4,NaiveFinder}("seq", 4, 33, STRAND_POS, 1, convert(LongSubSeq, dna"ATGCATGCATGCATGCTAGTAACTAGCTAG"), NamedTuple()),
+        ORF{4,NaiveFinder}("seq", 8, 22, STRAND_POS, 2, convert(LongSubSeq, dna"ATGCATGCATGCTAG"), NamedTuple()),
+        ORF{4,NaiveFinder}("seq", 12,29, STRAND_POS, 3, convert(LongSubSeq, dna"ATGCATGCTAGTAACTAG"), NamedTuple()),
+        ORF{4,NaiveFinder}("seq", 16,33, STRAND_POS, 1, convert(LongSubSeq, dna"ATGCTAGTAACTAGCTAG"), NamedTuple()) #
     ]
     
-    @test println(orfs) == println(orfstest)
+    @test println(orfs[1]) == println(orfstest[1])
     @test length(orfs) == 5
 end
 
@@ -62,35 +70,35 @@ end
     # On the other hand, the NCBI ORFfinder program predicts 13 ORFs whose length is greater than 75 nt and allowing alternative starts, from which one has an "outbound" stop
     seq = dna"TTCGTCAGTCGTTCTGTTTCATTCAATACGATAGTAATGTATTTTTCGTGCATTTCCGGTGGAATCGTGCCGTCCAGCATAGCCTCCAGATATCCCCTTATAGAGGTCAGAGGGGAACGGAAATCGTGGGATACATTGGCTACAAACTTTTTCTGATCATCCTCGGAACGGGCAATTTCGCTTGCCATATAATTCAGACAGGAAGCCAGATAACCGATTTCATCCTCACTATCGACCTGAAATTCATAATGCATATTACCGGCAGCATACTGCTCTGTGGCATGAGTGATCTTCCTCAGAGGAATATATACGATCTCAGTGAAAAAGATCAGAATGATCAGGGATAGCAGGAACAGGATTGCCAGGGTGATATAGGAAATATTCAGCAGGTTGTTACAGGATTTCTGAATATCATTCATATCAGTATGGATGACTACATAGCCTTTTACCTTGTAGTTGGAGGTAATGGGAGCAAATACAGTAAGTACATCCGAATCAAAATTACCGAAGAAATCACCAACAATGTAATAGGAGCCGCTGGTTACGGTCGAATCAAAATTCTCAATGACAACCACATTCTCCACATCTAAGGGACTATTGGTATCCAGTACCAGTCGTCCGGAGGGATTGATGATGCGAATCTCGGAATTCAGGTAGACCGCCAGGGAGTCCAGCTGCATTTTAACGGTCTCCAAAGTTGTTTCACTGGTGTACAATCCGCCGGCATAGGTTCCGGCGATCAGGGTTGCTTCGGAATAGAGACTTTCTGCCTTTTCCCGGATCAGATGTTCTTTGGTCATATTGGGAACAAAAGTTGTAACAATGATGAAACCAAATACACCAAAAATAAAATATGCGAGTATAAATTTTAGATAAAGTGTTTTTTTCATAACAAATCCTGCTTTTGGTATGACTTAATTACGTACTTCGAATTTATAGCCGATGCCCCAGATGGTGCTGATCTTCCAGTTGGCATGATCCTTGATCTTCTC"
     orfs = findorfs(seq, finder=NaiveFinder, minlen=75, alternative_start=true)
-    @test length(orfs) == 20
+    @test length(orfs) == 26 #20 
 
     orfstest = [
-                        # |- oubounds notation for the NCBI ORFfinder 
-        #               (>3:890, '-', 3)      NCBI-ORFfinder
-        ORF{NaiveFinder}(37:156, '+', 1),   # NCBI-ORFfinder
-        ORF{NaiveFinder}(48:347, '+', 3),   # Pyrodigal & NCBI-ORFfinder
-        ORF{NaiveFinder}(67:156, '+', 1),   
-        #               (153:347, '+', 3)     NCBI-ORFfinder 
-        ORF{NaiveFinder}(126:347, '+', 3),  
-        ORF{NaiveFinder}(182:289, '+', 2),  # NCBI-ORFfinder
-        ORF{NaiveFinder}(194:268, '-', 2),   
-        ORF{NaiveFinder}(194:283, '-', 2),  # NCBI-ORFfinder
-        ORF{NaiveFinder}(249:347, '+', 3),  
-        ORF{NaiveFinder}(286:375, '+', 1),  
-        #               (405:590, '+', 3)     NCBI-ORFfinder
-        ORF{NaiveFinder}(426:590, '+', 3),  # Pyrodigal
-        ORF{NaiveFinder}(446:520, '-', 2),
-        ORF{NaiveFinder}(446:523, '-', 2),
-        #               (538:657, '+', 1)     NCBI-ORFfinder
-        ORF{NaiveFinder}(565:657, '+', 1),
-        ORF{NaiveFinder}(650:727, '-', 2),
-        #               (675:872, '+', 3)     NCBI-ORFfinder
-        ORF{NaiveFinder}(698:820, '+', 2),
-        ORF{NaiveFinder}(746:820, '+', 2),
-        ORF{NaiveFinder}(786:872, '+', 3),
-        ORF{NaiveFinder}(793:876, '+', 1),
-        ORF{NaiveFinder}(802:876, '+', 1),
-        ORF{NaiveFinder}(887:976, '-', 2),
+                         "seq", # |- oubounds notation for the NCBI ORFfinder 
+        #                "seq", (>3:890, '-', 3)     NCBI-ORFfinder
+        ORF{NaiveFinder}("seq", 37:156, '+', 1),   # NCBI-ORFfinder
+        ORF{NaiveFinder}("seq", 48:347, '+', 3),   # Pyrodigal & NCBI-ORFfinder
+        ORF{NaiveFinder}("seq", 67:156, '+', 1),   
+        #               ("seq", 153:347, '+', 3)     NCBI-ORFfinder 
+        ORF{NaiveFinder}("seq", 126:347, '+', 3),  
+        ORF{NaiveFinder}("seq", 182:289, '+', 2),  # NCBI-ORFfinder
+        ORF{NaiveFinder}("seq", 194:268, '-', 2),   
+        ORF{NaiveFinder}("seq", 194:283, '-', 2),  # NCBI-ORFfinder
+        ORF{NaiveFinder}("seq", 249:347, '+', 3),  
+        ORF{NaiveFinder}("seq", 286:375, '+', 1),  
+        #               ("seq", 405:590, '+', 3)     NCBI-ORFfinder
+        ORF{NaiveFinder}("seq", 426:590, '+', 3),  # Pyrodigal
+        ORF{NaiveFinder}("seq", 446:520, '-', 2),
+        ORF{NaiveFinder}("seq", 446:523, '-', 2),
+        #               ("seq", 538:657, '+', 1)     NCBI-ORFfinder
+        ORF{NaiveFinder}("seq", 565:657, '+', 1),
+        ORF{NaiveFinder}("seq", 650:727, '-', 2),
+        #               ("seq", 675:872, '+', 3)     NCBI-ORFfinder
+        ORF{NaiveFinder}("seq", 698:820, '+', 2),
+        ORF{NaiveFinder}("seq", 746:820, '+', 2),
+        ORF{NaiveFinder}("seq", 786:872, '+', 3),
+        ORF{NaiveFinder}("seq", 793:876, '+', 1),
+        ORF{NaiveFinder}("seq", 802:876, '+', 1),
+        ORF{NaiveFinder}("seq", 887:976, '-', 2),
     ]
 
     @test println(orfs) == println(orfstest)
@@ -98,14 +106,7 @@ end
 
 @testitem "NaiveFinder lambda" begin
     cd(@__DIR__) # Required to find the fasta file
-
     using BioSequences, GeneFinder, FASTX
-
-    function fasta2bioseq(input::AbstractString)::Vector{LongSequence{DNAAlphabet{4}}}
-        FASTAReader(open(input)) do reader
-            return [LongSequence{DNAAlphabet{4}}(FASTX.sequence(record)) for record in reader]
-        end
-    end
 
     # Lambda phage tests
     # Compare to https://github.com/jonas-fuchs/viral_orf_finder/blob/master/orf_finder.py 
@@ -116,9 +117,11 @@ end
     # findorfs (GeneFinder.jl) --> 885
     # NCBI ORFfinder --> 375 ORFs
     # orfipy --> 375 (`orfipy NC_001416.1.fasta --start ATG --include-stop --min 75`)
-
-    lambda = fasta2bioseq("data/NC_001416.1.fasta")[1]
-    lambdaorfs = findorfs(lambda, finder=NaiveFinder, minlen=75)
+    
+    lambdafile = "data/NC_001416.1.fasta"
+    lambdarecord = open(collect, FASTA.Reader, lambdafile)[1]
+    lambdaseq = FASTX.sequence(LongDNA{4}, lambdarecord)
+    lambdaorfs = findorfs(lambdaseq, finder=NaiveFinder, minlen=75)
     
     @test length(lambdaorfs) == 885
 end
