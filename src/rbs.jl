98db5dbaadc431eded5d42a2af7b10645f44f937
Camilo Garcia <ca.garcia2@uniandes.edu.co>
2024-11-29 17:41:32 -0500

    Refactor RBS module to streamline functions and improve motif handling

--- src/rbs.jl
+++ src/rbs.jl
@@ -37,18 +37,7 @@ const RBSMOTIFS = Dict(
     # ExactSearchQuery(dna"ATACG", iscompatible)  => 5, ## testing motif
 )
 
-# export RBSMOTIFS, RBSQUERYMOTIGS
-# RBSMOTIFS = (
-#     dna"GGAGGA", dna"GGAGG", dna"GAGGA", dna"GGACGA", dna"GGATGA", dna"GGAAGA", dna"GGCGGA", dna"GGGGGA", dna"GGTGGA",
-#     dna"GGAG", dna"GAGG", dna"AGGA", dna"GGTGG", dna"GGGGG", dna"GGCGG", dna"AGG", dna"GAG", dna"GGA", 
-#     dna"GAAGA", dna"GATGA", dna"GACGA"
-# )
 
-# RBSQUERYMOTIFS = ExactSearchQuery.(RBSMOTIFS, iscompatible)
-
-
-# RBSQUERYMOTIGS = ExactSearchQuery.(RBSMOTIFS, iscompatible)
-    
 # ExactSearchQuery(dna"ATACG", iscompatible)  => 5, ## testing motif
 
 #       -20  -15  -10  -5    |-> start codon
@@ -69,29 +58,48 @@ const RBSMOTIFS = Dict(
 #       ...|......|..........ATG...
 
 export _rbswindows
-function _rbswindows(seq::SeqOrView{A}, orf::ORFI{N,F}) where {A,N,F}
-
-    windowa = orf.strand == STRAND_POS ? (orf.first-16:orf.first-5) : (orf.last+5:orf.last+16) 
+function _rbswindows(orf::ORFI{N,F}) where {N,F}
+    windowa = orf.strand == STRAND_POS ? (orf.first-16:orf.first-5) : (orf.last+5:orf.last+16)
     windowb = orf.strand == STRAND_POS ? (orf.first-10:orf.first-3) : (orf.last+3:orf.last+10)
     windowc = orf.strand == STRAND_POS ? (orf.first-18:orf.first-11) : (orf.last+11:orf.last+18)
-                                                                    # This is not searching the correct RBS in the reverse complemente strand
-    # windows = (a = windowa, b = windowb, c = windowc)
     windows = (windowa, windowb, windowc)
-    return windows#filter(window -> first(window) >= 1, windows)
+    return filter(window -> first(window) >= 1 && last(window) >= 1, windows)
 end
 
-export findrbs
-function findrbs(seq::SeqOrView{A}, orf::ORFI{N,F}) where {A,N,F}
+# export findrbs
+# function findrbs(seq::SeqOrView{A}, orf::ORFI{N,F}) where {A,N,F}
+#     # s = reverse(seq)
+#     score = 0
+#     wsymb = (:a, :b, :c)
+#     rbsvect = Vector{RBS}()
+#     windows = _rbswindows(orf)
+
+#     for i in 1:length(windows)
+#         window = windows[i]
+#         symbol = wsymb[i]
+#         sqv = @view seq[window]
+#         for (rbs, scr) in pairs(RBSMOTIFS)
+#             if occursin(rbs, sqv)
+#                 push!(rbsvect, RBS(rbs.seq, window, scr, symbol))
+#                 # score += scr  # Use the value associated with the key
+#             end
+#         end
+#     end
+#     return rbsvect
+# end
+
+export _findrbs
+function _findrbs(orf::ORFI{N,F}) where {N,F}
     # s = reverse(seq)
     score = 0
-    wsymb = (:a, :b, :c)
+    wsymb = (:A, :B, :C)
     rbsvect = Vector{RBS}()
-    windows = _rbswindows(seq, orf)
+    windows = _rbswindows(orf)
 
     for i in 1:length(windows)
         window = windows[i]
         symbol = wsymb[i]
-        sqv = @view seq[window]
+        sqv = @view source(orf)[window]
         for (rbs, scr) in pairs(RBSMOTIFS)
             if occursin(rbs, sqv)
                 push!(rbsvect, RBS(rbs.seq, window, scr, symbol))
@@ -102,17 +110,48 @@ function findrbs(seq::SeqOrView{A}, orf::ORFI{N,F}) where {A,N,F}
     return rbsvect
 end
 
-function orf_rbs_score(seq::SeqOrView{A}, orf::ORFI{N,F}) where {A,N,F}
+# export orf_rbs_score
+# function orf_rbs_score(seq::SeqOrView{A}, orf::ORFI{N,F}) where {A,N,F}
+#     # Initialize the score and the max scores dictionary
+#     wsymb = (:a, :b, :c)
+#     windows = _rbswindows(orf)
+#     maxscores = Dict(:a => 0, :b => 0, :c => 0)
+
+#     # Iterate over the windows and their corresponding symbols
+#     @inbounds for i in 1:length(windows)
+#         window = windows[i]
+#         symbol = wsymb[i]
+#         sqv = @view seq[window]
+
+#         # Check for RBS motifs in the sequence view
+#         @inbounds for (rbs, scr) in pairs(RBSMOTIFS)
+#             if occursin(rbs, sqv)
+#                 # Update the max score directly
+#                 if scr > maxscores[symbol]
+#                     maxscores[symbol] = scr
+#                 end
+#             end
+#         end
+#     end
+
+#     # Sum the maximum scores of each window
+#     total_score = sum(values(maxscores))
+    
+#     return total_score
+# end
+
+export _orf_rbs_score
+function _orf_rbs_score(orf::ORFI{N,F}) where {N,F}
     # Initialize the score and the max scores dictionary
     wsymb = (:a, :b, :c)
-    windows = _rbswindows(seq, orf)
+    windows = _rbswindows(orf)
     maxscores = Dict(:a => 0, :b => 0, :c => 0)
 
     # Iterate over the windows and their corresponding symbols
     @inbounds for i in 1:length(windows)
         window = windows[i]
         symbol = wsymb[i]
-        sqv = @view seq[window]
+        sqv = @view source(orf)[window]
 
         # Check for RBS motifs in the sequence view
         @inbounds for (rbs, scr) in pairs(RBSMOTIFS)
@@ -131,9 +170,28 @@ function orf_rbs_score(seq::SeqOrView{A}, orf::ORFI{N,F}) where {A,N,F}
     return total_score
 end
 
+export motifseq
+function motifseq(rbs::RBS)
+    return rbs.motif
+end
+
+export offset
+function offset(rbs::RBS)
+    return rbs.offset
+end
 
 ### Another implementation
 
+# export RBSMOTIFS, RBSQUERYMOTIGS
+# RBSMOTIFS = (
+#     dna"GGAGGA", dna"GGAGG", dna"GAGGA", dna"GGACGA", dna"GGATGA", dna"GGAAGA", dna"GGCGGA", dna"GGGGGA", dna"GGTGGA",
+#     dna"GGAG", dna"GAGG", dna"AGGA", dna"GGTGG", dna"GGGGG", dna"GGCGG", dna"AGG", dna"GAG", dna"GGA", 
+#     dna"GAAGA", dna"GATGA", dna"GACGA"
+# )
+
+# RBSQUERYMOTIFS = ExactSearchQuery.(RBSMOTIFS, iscompatible)
+# RBSQUERYMOTIGS = ExactSearchQuery.(RBSMOTIFS, iscompatible)
+    
 
 export RBSMOTIFS2
 RBSMOTIFS2 = (
@@ -182,20 +240,8 @@ RBSMOTIFS2 = (
     RBS(dna"AGG", 3:6, 1, :Z)
 )
 
-export motifseq
-function motifseq(rbs::RBS)
-    return rbs.motif
-end
-
-export RBSMOTIFS2QUERY
-RBSMOTIFS2QUERY = ExactSearchQuery.(motifseq.(RBSMOTIFS2))
-
-export offset
-function offset(rbs::RBS)
-    return rbs.offset
-end
-
-function motifscore(seq::SeqOrView{A}) where {A}
+export _motifscore
+function _motifscore(seq::SeqOrView{A}) where {A}
     score = 0
     for rbs in RBSMOTIFS2
         if occursin(rbs.motif, seq)
@@ -205,17 +251,23 @@ function motifscore(seq::SeqOrView{A}) where {A}
     return score
 end
 
+export RBSMOTIFS2QUERY
+RBSMOTIFS2QUERY = ExactSearchQuery.(motifseq.(RBSMOTIFS2))
 
-export orf_rbs_score2
-function orf_rbs_score2(seq::SeqOrView{A}, orf::ORFI{N,F}) where {A,N,F}
+export _findrbs2
+function _findrbs2(orf::ORFI{N,F}) where {N,F}
+    rbsvect = Vector{RBS}()#Vector{RBS}()
     for i in 1:length(RBSMOTIFS2)
         rbs = RBSMOTIFS2[i]
         rbsq = RBSMOTIFS2QUERY[i]
-        println(rbs)
-        return findprev(rbsq, seq, orf.first)
+        match = findprev(rbsq, source(orf), i) #needs to deal with strand
+        if match != nothing
+         push!(rbsvect, RBS(rbs.motif, -(match, rbs.offset), rbs.score, rbs.window))
+         println("Match found at: ", match)
+        end
     end
+    return rbsvect
 end
-
 
 # An idea of another implemetation of the a orf finder would be to use qstart = ExactSearchQuery(dna"ATG") 
 # and then findall the start codons in the sequence and reverse sequence appended (seq * reverse_complement(seq))
