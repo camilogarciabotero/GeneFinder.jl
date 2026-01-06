2555e41a6b773b8a8cf713283f50ff929d7a5809
Camilo García <ca.garcia2@uniandes.edu.co>
2026-01-06 17:27:03 -0500

    Add finder function to retrieve the GeneFinderMethod from ORF

--- src/types.jl
+++ src/types.jl
@@ -155,6 +155,8 @@ function strand(i::ORF{F}) where {F}
     return i.strand
 end
 
+finder(i::ORF{F}) where {F} = F
+
 function Base.show(io::IO, i::ORF{F}) where {F}
     print(io, "ORF{", finder(i), "}(", leftposition(i), ":", rightposition(i), ", '")
     show(io, strand(i))
