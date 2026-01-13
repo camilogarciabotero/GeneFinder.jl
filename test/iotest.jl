5ff9373e3eac6833a711574830c49164a7fc44dd
Camilo García <ca.garcia2@uniandes.edu.co>
2026-01-13 13:37:44 -0500

    Refactor iotest.jl to improve test coverage and organization, including ORFCollection API tests and file writing validations for FNA, FAA, BED, and GFF formats

--- test/iotest.jl
+++ test/iotest.jl
@@ -1,42 +1,68 @@
-@testset "writting methods" begin
-    cd(@__DIR__) # Required to find the fasta file 
+@testset "Writing methods" begin
+    cd(@__DIR__)
     using BioSequences, GeneFinder, FASTX
-# # 
-#     # Test case 1 fna
-# # 
-#     # From pyrodigal issue #13 link: https://github.com/althonos/pyrodigal/blob/1f939b0913b48dbaa55d574b20e124f1b8323825/pyrodigal/tests/test_orf_finder.py#L271
-#     # Pyrodigal predicts 2 genes from this sequence:
-#     # 1) An alternative start codon (GTG) sequence at 48:347
-#     # 2) A common start codon sequence at 426:590
-#     # On the other hand, the NCBI ORFIfinder program predicts 9 ORFIs whose length is greater than 75 nt, from which one has an "outbound" stop
+
     seq = dna"TTCGTCAGTCGTTCTGTTTCATTCAATACGATAGTAATGTATTTTTCGTGCATTTCCGGTGGAATCGTGCCGTCCAGCATAGCCTCCAGATATCCCCTTATAGAGGTCAGAGGGGAACGGAAATCGTGGGATACATTGGCTACAAACTTTTTCTGATCATCCTCGGAACGGGCAATTTCGCTTGCCATATAATTCAGACAGGAAGCCAGATAACCGATTTCATCCTCACTATCGACCTGAAATTCATAATGCATATTACCGGCAGCATACTGCTCTGTGGCATGAGTGATCTTCCTCAGAGGAATATATACGATCTCAGTGAAAAAGATCAGAATGATCAGGGATAGCAGGAACAGGATTGCCAGGGTGATATAGGAAATATTCAGCAGGTTGTTACAGGATTTCTGAATATCATTCATATCAGTATGGATGACTACATAGCCTTTTACCTTGTAGTTGGAGGTAATGGGAGCAAATACAGTAAGTACATCCGAATCAAAATTACCGAAGAAATCACCAACAATGTAATAGGAGCCGCTGGTTACGGTCGAATCAAAATTCTCAATGACAACCACATTCTCCACATCTAAGGGACTATTGGTATCCAGTACCAGTCGTCCGGAGGGATTGATGATGCGAATCTCGGAATTCAGGTAGACCGCCAGGGAGTCCAGCTGCATTTTAACGGTCTCCAAAGTTGTTTCACTGGTGTACAATCCGCCGGCATAGGTTCCGGCGATCAGGGTTGCTTCGGAATAGAGACTTTCTGCCTTTTCCCGGATCAGATGTTCTTTGGTCATATTGGGAACAAAAGTTGTAACAATGATGAAACCAAATACACCAAAAATAAAATATGCGAGTATAAATTTTAGATAAAGTGTTTTTTTCATAACAAATCCTGCTTTTGGTATGACTTAATTACGTACTTCGAATTTATAGCCGATGCCCCAGATGGTGCTGATCTTCCAGTTGGCATGATCCTTGATCTTCTC"
     
+    # Test ORFCollection API
+    collection = NaiveFinder(seq)
+    
+    @test length(collection) > 0
+    @test !isempty(collection)
+    
+    # Test sequence extraction
+    first_seq = GeneFinder.sequence(collection, 1)
+    @test first_seq isa LongSubSeq{DNAAlphabet{4}}
+    
+    # Test iteration
+    for orf in collection
+        @test orf isa ORF
+        @test leftposition(orf) >= 1
+        @test rightposition(orf) <= length(seq)
+    end
+
+    # Test FNA writing
     seqfna = "data/out-seq.fna"
     open(seqfna, "w") do io
-        write_orfs_fna(seq, io, finder=NaiveFinder)
+        write_orfs_fna(seq, io; finder=NaiveFinder)
     end
-# # 
-    # seqfnarecords = open(collect, FASTA.Reader, "data/out-seq.fna")
-# # 
-    # @test seqfnarecords[1] == FASTX.FASTA.Record("seq id=01 start=5 stop=22 strand=- frame=2 score=0", "ATGAAACAGAACGACTGA")
-#     @test length(seqfnarecords) == 32
-#     @test identifier(seqfnarecords[1]) == "unnamedseq"
-#     @test description(seqfnarecords[1]) == "unnamedseq id=01 start=5 stop=22 strand=- frame=2 score=0.0"
-#     @test FASTX.sequence(seqfnarecords[1]) == "ATGAAACAGAACGACTGA"
-# # 
-#     # Test case 2 faa
+    
+    fnarecords = open(collect, FASTA.Reader, seqfna)
+    @test length(fnarecords) == length(collection)
+    @test !isempty(FASTX.identifier(fnarecords[1]))
+    
+    # Test FAA writing
+    seqfaa = "data/out-seq.faa"
+    open(seqfaa, "w") do io
+        write_orfs_faa(seq, io; finder=NaiveFinder)
+    end
+    
+    faarecords = open(collect, FASTA.Reader, seqfaa)
+    @test length(faarecords) == length(collection)
+    
+    # Test BED writing
+    seqbed = "data/out-seq.bed"
+    open(seqbed, "w") do io
+        write_orfs_bed(seq, io; finder=NaiveFinder)
+    end
+    
+    bedlines = readlines(seqbed)
+    @test length(bedlines) == length(collection)
+    
+    # Test GFF writing
+    seqgff = "data/out-seq.gff"
+    open(seqgff, "w") do io
+        write_orfs_gff(seq, io; finder=NaiveFinder)
+    end
+    
+    gfflines = readlines(seqgff)
+    # GFF has 2 header lines + ORF entries
+    @test length(gfflines) == length(collection) + 2
+    @test startswith(gfflines[1], "##gff-version")
 
-#     seqfaa = "data/out-seq.faa"
-#     open(seqfaa, "w") do io
-#         write_orfs_faa(seq, io, finder=NaiveFinder)
-#     end
-# # 
-#     seqfaarecords = open(collect, FASTA.Reader, "data/out-seq.faa")
-# # 
-#     @test seqfaarecords[2] == FASTX.FASTA.Record("unnamedseq id=02 start=37 stop=156 strand=+ frame=1 score=0.0", "MYFSCISGGIVPSSIASRYPLIEVRGERKSWDTLATNFF*")
-#     @test length(seqfaarecords) == 32
-#     @test identifier(seqfaarecords[2]) == "unnamedseq"
-#     @test description(seqfaarecords[2]) == "unnamedseq id=02 start=37 stop=156 strand=+ frame=1 score=0.0"
-#     @test FASTX.sequence(seqfaarecords[2]) == "MYFSCISGGIVPSSIASRYPLIEVRGERKSWDTLATNFF*"
-
+    # # Cleanup
+    rm(seqfna, force=true)
+    rm(seqfaa, force=true)
+    rm(seqbed, force=true)
+    rm(seqgff, force=true)
 end
