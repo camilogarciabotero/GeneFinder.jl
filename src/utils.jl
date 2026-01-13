6ac102b3fae31b9cb3d8ef099a0249fde75aa815
Camilo García <ca.garcia2@uniandes.edu.co>
2026-01-13 15:52:38 -0500

    Refactor utils.jl by removing unused functions and improving code organization

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,5 @@
 # General purposes methods supporting main functions
+
 """
     _hasprematurestop(seq::NucleicSeqOrView{DNAAlphabet{N}})::Bool where {N}
 
@@ -37,71 +38,3 @@ function _hasprematurestop(seq::NucleicSeqOrView{DNAAlphabet{N}})::Bool where {N
 
     return false
 end
-
-"""
-    _orfseq(seq::LongSequence{DNAAlphabet{N}}, start::Int64, stop::Int64, strand::Strand) where {N} -> LongSubSeq{DNAAlphabet{N}}
-
-Extract the ORFI sequence from the `seq` of type `LongSequence{DNAAlphabet{N}}` given the `start` and `stop` positions and the `strand`.
-"""
-function _orfseq(seq::SeqOrView{DNAAlphabet{N}}, start::Int64, stop::Int64, strand::Strand) where {N}
-    if strand == STRAND_POS
-        return convert(LongSubSeq, seq[start:stop])
-    else
-        return convert(LongSubSeq, reverse_complement(seq[start:stop]))
-    end
-end
-
-"""
-    _varname(var::Any) -> String
-
-Get the name of the variable `var` in the current scope.
-"""
-function _varname(var::Any)
-    for name in names(Main)
-        try
-            if getfield(Main, name) === var
-                return string(name)
-            end
-        catch e
-            # Skip if getfield fails, e.g., for names that cannot be accessed directly
-            continue
-        end
-    end
-    return nothing
-end
-
-"""
-    _varsymbol(var::Any) -> Symbol
-
-Get the symbol of the variable `var` in the current scope.
-"""
-function _varsymbol(var::Any)
-    for name in names(Main)
-        try
-            if getfield(Main, name) === var
-                return Symbol(name)
-            end
-        catch e
-            # Skip if getfield fails, e.g., for names that cannot be accessed directly
-            continue
-        end
-    end
-    return nothing
-end
-
-# export location
-# function location(orf::ORFI{N,F})::UnitRange{Int64} where {N,F}
-#     return orf.first:orf.last
-# end
-
-# export _norfs
-# function _norfs(seq::NucleicSeqOrView{DNAAlphabet{N}}) where {N}
-#     cpseq = copy(seq)
-#     orfs = findorfs(cpseq; finder=NaiveCollector)
-
-#     for l in location.(orfs)
-#         deleteat!(cpseq, l)
-#     end
-
-#     return cpseq
-# end
