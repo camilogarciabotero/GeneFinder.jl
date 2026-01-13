f6eb3632daedc02bed9ae0f3f1f06123c35bcf77
Documenter.jl <documenter@juliadocs.github.io>
2026-01-13 22:57:16 +0000

    build based on cd6f37e

--- dev/assets/rbs.md.B_NAA8JX.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as A,c as G,o as C,ai as s}from"./chunks/framework.CATWNKgm.js";const k=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"rbs.md","filePath":"rbs.md","lastUpdated":null}'),i={name:"rbs.md"};function a(n,T,t,e,h,l){return C(),G("div",null,[...T[0]||(T[0]=[s("",18)])])}const r=A(i,[["render",a]]);export{k as __pageData,r as default};
